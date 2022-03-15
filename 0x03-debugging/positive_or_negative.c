67d537a266398619e05752cc632212feef2b8a7f
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-03-15 23:41:41 +0000

    positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - positive_or_negative
+ * 
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%d is positive\i", i);
+	else if (i == 0)
+		printf("%d is zero\i", i);
+	else if (i < 0)
+		printf("%d is negative\i", i);
+	}
+}
