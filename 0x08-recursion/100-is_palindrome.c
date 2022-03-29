8babbdbe94050e320e6e178f548699891df0dc8d
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-03-29 22:35:31 +0000

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,4 +22,10 @@ int palind2(char *a, int l)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
