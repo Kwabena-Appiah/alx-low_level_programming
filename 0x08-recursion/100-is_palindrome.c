05e96c8c82477b4754d7986bb9113888de205f87
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-03-29 22:37:50 +0000

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,6 +22,22 @@ int palind2(char *a, int l)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_palindrome(char *s)
 {
 	int l;
