744065dbaa737e54fdbbb2615632504b6b2945cf
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-04-13 14:46:25 +0000

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include "function_pointers.h"
 
 /**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
- * Return: Nothing.
- */
-void print_elem(int elem)
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
 {
-    printf("%d\n", elem);
-}
+	int (*oprt)(int, int);
 
-/**
- * print_elem_hex - prints an integer, in hexadecimal
- * @elem: the integer to print
- *
- * Return: Nothing.
- */
-void print_elem_hex(int elem)
-{
-    printf("0x%x\n", elem);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[5] = {0, 98, 402, 1024, 4096};
+	oprt = get_op_func(argv[2]);
 
-    array_iterator(array, 5, &print_elem);
-    array_iterator(array, 5, &print_elem_hex);
-    return (0);
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
