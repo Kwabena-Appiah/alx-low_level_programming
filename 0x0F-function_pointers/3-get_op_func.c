e44204b8b277ca72d4e39bf9f2bb9fb33326511a
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-04-13 15:01:00 +0000

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,34 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - function pointer
- * @s: char type string
- *
- * Description: struction with operators
- * Return: pointer to operator and function
- */
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (*s == *ops[i].op)
-		{
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		}
+
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
+
+	return (0);
 }
