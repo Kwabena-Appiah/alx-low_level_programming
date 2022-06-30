a215d8d595c5942d935031a07de53e1dbf89569d
Kwabena-Appiah <ofosuappiahgodfred@gmail.com>
2022-06-30 19:06:50 +0000

    delete

--- 0x1A-hash_tables/2-key_index.c
+++
@@ -1,9 +0,0 @@
-#include "hash_tables.h"
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-  if (key == NULL || size == 0)
-    return 0;
-  else
-    return (hash_djb2(key) % size);
-}
